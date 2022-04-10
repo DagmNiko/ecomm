4f69e90160abc9bbd7dbae1a6a485c47309d9d94
Dagm Niko <dagmniko79@gmail.com>
2022-04-10 16:24:01 +0300

    upload files to my repository

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+
+
+export default function App() {
+  const click = () => {
+    <Text>clicked!</Text>
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={click}>
+        <Image source={require("./assets/favicon.png")}/>
+      </TouchableOpacity>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+   
+  },
+});
