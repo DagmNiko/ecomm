ee13a4dc1020963c26a81cdc2a4312f194aacb70
Dagm Niko <dagmniko79@gmail.com>
2022-04-17 22:11:01 +0300

    format App.js

--- App.js
+++ App.js
@@ -1,27 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import {
+  StatusBar
+} from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity
+} from 'react-native';
 
 
 export default function App() {
-  const click = () => {
-    <Text>clicked!</Text>
+  const click = () => { < Text > clicked!</Text >
   }
-  return (
-    <SafeAreaView style={styles.container}>
-      <TouchableOpacity onPress={click}>
-        <Image source={require("./assets/favicon.png")}/>
-      </TouchableOpacity>
-      
-    </SafeAreaView>
+  return ( < SafeAreaView style = {
+    styles.container
+  } > < TouchableOpacity onPress = {
+    click
+  } > < Image source = {
+    require("./assets/favicon.png")}/ > < /TouchableOpacity > < /SafeAreaView >
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-   
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center',
+
+    },
+  });
