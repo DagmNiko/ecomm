341215232f2be61168d3961fda1a33d1474d9ac1
Dagm Niko <dagmniko79@gmail.com>
2022-04-17 22:34:27 +0300

    add nav buttons

--- Components/index.jsx
+++ Components/index.jsx
@@ -1,3 +1,46 @@
 import React, {
   Component
 } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Button
+} from 'react-native';
+
+class NavBtn() {
+  state = {}
+  return (
+    <View>
+      <NearBy />
+      <WorldWide />
+    </View>
+  )
+}
+const NearBy = () => {
+  <Button style={styles.nearby}>Near by</Button>
+}
+
+const WorldWide = () => {
+  <Button style={styles.worldwide}>Near by</Button>
+}
+
+const styles = StyleSheet.create({
+  nearby: {
+    border: '1px solid red',
+    width: '30rem',
+    borderRadius: 10
+  },
+  worldwide: {
+    border: '1px solid blue',
+    width: '30rem',
+    borderRadius: 10
+  },
+});
+
+export default NavBtn;
+
+
